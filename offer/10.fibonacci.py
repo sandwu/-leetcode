5d16602ce470bb6ec40bc0accb756ce91af5fc68
sandwu <wuxinlingwxl@163.com>
2021-05-31 10:14:52 +0800

    Update 10.fibonacci.py

--- offer/10.fibonacci.py
+++ offer/10.fibonacci.py
@@ -1,13 +1,25 @@
 
 
 class Solution:
+    def __init__(self):
+        self.map = {}
+
     def fibonacci(self, n):
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-        return self.fibonacci(n-1) + self.fibonacci(n-2)
+        if self.map.get(n):
+            return self.map[n]
+        if n <= 1:
+            return n
+        result = self.fibonacci(n-1) + self.fibonacci(n-2)
+        self.map[n] = result
+        return result
+
+    def fibonacci2(self, n):
+        a, b = 0, 1
+        for _ in range(n):
+            a, b = b, a+b
+        return a % 1000000007
 
 
 s = Solution()
-print(s.fibonacci(5))
+print(s.fibonacci(10))
+print(s.fibonacci2(10))
